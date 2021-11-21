ff5b3884e156ffd2391d62728c740a1826114349
NinaScot <NinaScot@users.noreply.github.com>
2021-11-21 20:24:05 +0000

    deploy: 1059c6fa499e8079bbf7898a7737d99232e92fd2

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
+// animation
+var i = 0;
+var txt =
+  "An interactive, informational site about the life cycle of our favorite red and spotted beetles."; /* The text */
+var speed = 50; /* The speed/duration of the effect in milliseconds */
 
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("ladybug_intro_text").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
