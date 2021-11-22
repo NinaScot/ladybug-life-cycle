899198f6a996eda4cc470819663400e22a592088
NinaScot <NinaScot@users.noreply.github.com>
2021-11-22 20:27:11 +0000

    deploy: 00487109bf163bb08fd19d4e14b0b4683d568293

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 // animation
+/* define things */
 var i = 0;
 var txt =
   "An interactive, informational site about the life cycle of our favorite red and spotted beetles.";
 
 var speed = 50; /* The speed/duration of the effect in milliseconds */
-
+/* make function */
 function typewriter() {
   if (i < txt.length) {
     document.getElementById("ladybug_intro_text").innerHTML += txt.charAt(i);
@@ -15,13 +16,75 @@ function typewriter() {
 typewriter();
 
 //cursor
-
+/* define things */
 let html = document.querySelector("html");
 let div = document.querySelector("div");
-
+/* make function */
 function logMouse(e) {
   console.log(e.clientX, e.clientY);
   div.style = `top: ${e.clientY}px; left: ${e.clientX}px`;
 }
 
 html.addEventListener("mousemove", logMouse);
+
+// Changing Slides
+/* define things */
+let slideIndex = 0;
+let slides = [
+  {
+    completedAction: false,
+  },
+  {
+    completedAction: false,
+  },
+  {
+    completedAction: false,
+  },
+];
+let slideElements = document.querySelectorAll(".slide");
+
+// slideAction()
+function slideAction() {
+  console.log("Doing Slide Action");
+  slides[slideIndex].completedAction = true;
+  slideElements[slideIndex].classList.add("completed-action");
+}
+
+// next() check to see if action is complete, if so, navigate to next slide
+function next() {
+  if (slides[slideIndex].completedAction) {
+    slideElements[slideIndex].classList.toggle("active");
+
+    if (slideIndex + 1 > slideElements.length - 1) {
+      reset();
+    } else {
+      slideIndex++;
+    }
+
+    slideElements[slideIndex].classList.toggle("active");
+
+    render();
+  } else {
+    console.log("You must do the action before going next.");
+  }
+}
+
+// render() represent the change in state visually
+function render() {
+  document.querySelector("#slide-indicator").innerHTML = slideIndex;
+}
+
+function reset() {
+  slideIndex = 0;
+  slides.forEach((slide) => {
+    slide.completedAction = false;
+  });
+  slideElements.forEach((slideElement) => {
+    slideElement.classList.remove("completed-action");
+  });
+}
+
+document
+  .querySelector("#do-slide-action")
+  .addEventListener("click", slideAction);
+document.querySelector("#slide-next").addEventListener("click", next);
